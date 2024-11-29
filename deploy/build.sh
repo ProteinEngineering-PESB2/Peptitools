ad20130eaa7aad91adb7ace666bf22f70ef1b414
Diego Alvarez S. <dialvarezs@gmail.com>
2024-11-29 10:20:11 -0300

    Pull always on build

--- deploy/build.sh
+++ deploy/build.sh
@@ -2,5 +2,5 @@
 
 date=$(date '+%Y%m%d')
 
-podman build -t peptitools_backend:"$date" -t peptitools_backend:latest ../backend/
-podman build -t peptitools_frontend:"$date" -t peptitools_frontend:latest ../frontend/
+podman build --pull always -t peptitools_backend:"$date" -t peptitools_backend:latest ../backend/
+podman build --pull always -t peptitools_frontend:"$date" -t peptitools_frontend:latest ../frontend/
